1f722efef0f5f5400ee4bd3df2e6e520a986662a
Brandon <Brandon@brandons-pro.gateway.2wire.net>
2014-03-15 19:24:40 -0400

    Styled Project and fixed layout

--- gen/com/rawrlikeme/pig/R.java
+++ gen/com/rawrlikeme/pig/R.java
@@ -30,9 +30,10 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
+        public static final int actionReset=0x7f080008;
         public static final int btnHold=0x7f080000;
         public static final int btnRoll=0x7f080001;
-        public static final int imageView1=0x7f080007;
+        public static final int imgDice=0x7f080007;
         public static final int txtPlayerOne=0x7f080003;
         public static final int txtPlayerOneScore=0x7f080005;
         public static final int txtPlayerTwo=0x7f080004;
@@ -51,8 +52,10 @@ public final class R {
         public static final int btnHold=0x7f050004;
         public static final int btnRoll=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int menuReset=0x7f050008;
         public static final int txtPlayerOne=0x7f050005;
         public static final int txtPlayerTwo=0x7f050006;
+        public static final int txtReady=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/rawrlikeme/pig/MainActivity.java
+++ src/com/rawrlikeme/pig/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -17,11 +18,13 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		// Declare our View variables
-		final TextView PlayerOneScoreLabel = (TextView) findViewById(R.id.txtPlayerOneScore);
-		final TextView PlayerTwoScoreLabel = (TextView) findViewById(R.id.txtPlayerTwoScore);
-		final Button HoldButton = (Button) findViewById(R.id.btnHold);
-		final Button RollButton = (Button) findViewById(R.id.btnRoll);
+		final TextView PlayerOneScoreLabel = (TextView) findViewById(R.id.txtPlayerOneScore); // TextView for Player1's score
+		final TextView PlayerTwoScoreLabel = (TextView) findViewById(R.id.txtPlayerTwoScore); // TextView for Player2's score
+		final Button HoldButton = (Button) findViewById(R.id.btnHold); // Button for the Hold method
+		final Button RollButton = (Button) findViewById(R.id.btnRoll); // Button for the Roll method
+		final ImageView DiceImage = (ImageView) findViewById(R.id.imgDice); // ImageView to display the Dice
 		
+		// RollButton onClickListener to call the other Function
 		RollButton.setOnClickListener(new View.OnClickListener() {
 			// What to do when the user clicks 'Roll'
 			public void onClick(View arg0) {
